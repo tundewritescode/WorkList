fe9b44d2c2a6607ef7167dd39e250a2ed1d25eb0
tundewritescode <tundewrites@gmail.com>
2017-10-22 00:10:52 +0100

    create signin controller

--- server/controllers/UserController.js
+++ server/controllers/UserController.js
@@ -70,6 +70,74 @@ class UserController {
       });
     }
   }
+
+  /**
+   * Signs in a new user
+   *
+   * @static
+   * @param {object} request - request object
+   * @param {object} response - response object
+   * @memberof UserController
+   *
+   * @returns {void}
+   */
+  static async signIn(request, response) {
+    try {
+      request.checkBody('email', 'Email is required').isEmail();
+      request.checkBody('password', 'Password is required').notEmpty();
+
+      const requestErrors = request.validationErrors();
+
+      if (requestErrors) {
+        response.status(400).json({
+          errors: requestErrors,
+        });
+      } else {
+        const existingUser = await User.findOne({ email: request.body.email });
+
+        if (existingUser) {
+          const passwordMatches = await bcrypt.compare(
+            request.body.password,
+            existingUser.password
+          );
+
+          const {
+            _id,
+            firstName,
+            lastName,
+            email,
+          } = existingUser;
+          if (passwordMatches) {
+            response.status(200).json({
+              userData: {
+                _id,
+                firstName,
+                lastName,
+                email,
+              },
+              token: await jwt.sign(
+                { _id, email },
+                process.env.SECRET_KEY,
+                { expiresIn: '1h' }
+              )
+            });
+          } else {
+            response.status(401).json({
+              error: 'Username and password do not match'
+            });
+          }
+        } else {
+          response.status(404).json({
+            error: 'User not found'
+          });
+        }
+      }
+    } catch (error) {
+      response.status(500).json({
+        error: 'Oops! Something broke'
+      });
+    }
+  }
 }
 
 export default UserController;
--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ import UserController from './../controllers/UserController';
 
 const userRoutes = (versionURL, app) => {
   app.post(`${versionURL}/users/signup`, UserController.signUp);
+  app.post(`${versionURL}/users/signin`, UserController.signIn);
 };
 
 export default userRoutes;
